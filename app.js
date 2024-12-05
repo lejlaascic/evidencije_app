fa7286f99f849f4436f807b410f69c399fb18ae6
Lejla Ascic <lejlaascic86@gmail.com>
2024-12-05 14:52:45 +0100

    changed

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const http = require('http');
+
+const server = http.createServer(function(req,res){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+const port = 3000;
+const hostname = 'localhost';
+
+
+server.listen (port, hostname,() =>{
+    console.log(`Server is running at http://${hostname}:${port}`);
+})
